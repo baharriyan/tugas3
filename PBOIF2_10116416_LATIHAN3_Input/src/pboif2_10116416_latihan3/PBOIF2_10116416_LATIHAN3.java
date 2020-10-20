18f405f9fdaf91a36775ff2ba4281215e389f9f1
baharriyan <indonesiariyan@gmail.com>
2020-10-20 18:50:59 +0700

    first commit

---
+++ PBOIF2_10116416_LATIHAN3_Input/src/pboif2_10116416_latihan3/PBOIF2_10116416_LATIHAN3.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116416_latihan3;
+import java.util.Scanner;
+package PBOIF2_10116416_LATIHAN2;
+
+/**
+ *
+ * @author Riyan Bahar
+ * Nama  : Riyan Bahar
+ * Kelas : IF-2
+ * Nim   : 10116416
+ * 
+ */
+public class PBOIF2_10116416_LATIHAN3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.print("Masukkan nama anda: ");
+        Scanner scanner = new Scanner(System.in);
+        
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah " + nama);
+    }
+    
+}
